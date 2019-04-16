8bf6f3de449cc795d952b78614da7bfdca2d4015
Viennarz Curtiz <viennarzc@gmail.com>
2019-04-16 16:29:07 +0800

    Initial Commit

---
+++ VVCPhilippinesProviderPrefixes/VVCPhilippinesProviderPrefixes.h
@@ -0,0 +1,19 @@
+//
+//  VVCPhilippinesProviderPrefixes.h
+//  VVCPhilippinesProviderPrefixes
+//
+//  Created by SCI-Viennarz on 16/04/2019.
+//  Copyright © 2019 VVCSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VVCPhilippinesProviderPrefixes.
+FOUNDATION_EXPORT double VVCPhilippinesProviderPrefixesVersionNumber;
+
+//! Project version string for VVCPhilippinesProviderPrefixes.
+FOUNDATION_EXPORT const unsigned char VVCPhilippinesProviderPrefixesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VVCPhilippinesProviderPrefixes/PublicHeader.h>
+
+
