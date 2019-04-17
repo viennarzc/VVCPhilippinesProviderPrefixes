20fc0617c67488358967400135668cd92a09ee05
Viennarz Curtiz <viennarzc@gmail.com>
2019-04-17 11:49:46 +0800

    Add method to check network providers, Update telcos json file, Add Tests

--- VVCPhilippinesProviderPrefixes/VVCPHMobileNetwork.swift
+++ VVCPhilippinesProviderPrefixes/VVCPHMobileNetwork.swift
@@ -10,6 +10,15 @@ import Foundation
 class VVCMobileNetwork: NSObject {
   private(set) var providers: [VVCProvider] = []
 
+  enum NetworkProvider {
+    case globe
+    case tnt
+    case globetm
+    case smart
+    case sun
+    case globeprepaid
+  }
+
   override init() {
     super.init()
     initList()
@@ -19,17 +28,130 @@ class VVCMobileNetwork: NSObject {
     if let data = loadJson(filename: "test") {
       self.providers = data
     }
-    
+
+
+  }
+
+  public func provider(contactNumber: String) -> NetworkProvider? {
+
+    if isGlobe(number: contactNumber) {
+      return .globe
+      
+    } else if isSun(number: contactNumber) {
+      return .sun
+      
+    } else if isTNT(number: contactNumber) {
+      return .tnt
+      
+    } else if isGlobePrepaid(number: contactNumber) {
+      return .globeprepaid
+      
+    } else if isGlobeTM(number: contactNumber) {
+      return .globetm
+      
+    } else if isSmart(number: contactNumber) {
+      return .smart
+      
+    }
+
+    return nil
+  }
+
+
+  private func isGlobe(number: String) -> Bool {
+    let globe = self.providers.filter { $0.name == "globe" }
+    var number = number
+
+    if number.hasPrefix("09") {
+      number = String(number.dropFirst())
+    }
+
+    guard let g = globe.first else { return false }
+
+    return doMatch(prefixes: g.prefixes, with: number)
+  }
+
+  private func isSun(number: String) -> Bool {
+    let sun = self.providers.filter { $0.name == "sun" }
+    var number = number
+
+    if number.hasPrefix("09") {
+      number = String(number.dropFirst())
+    }
+
+    guard let s = sun.first else { return false }
+
+    return doMatch(prefixes: s.prefixes, with: number)
+  }
+
+  private func isSmart(number: String) -> Bool {
+    let smart = self.providers.filter { $0.name == "smart" }
+    var number = number
+
+    if number.hasPrefix("09") {
+      number = String(number.dropFirst())
+    }
+
+    guard let s = smart.first else { return false }
+
+    return doMatch(prefixes: s.prefixes, with: number)
+  }
+
+  private func isTNT(number: String) -> Bool {
+    let tnt = self.providers.filter { $0.name == "tnt" }
+    var number = number
+
+    if number.hasPrefix("09") {
+      number = String(number.dropFirst())
+    }
+
+    guard let s = tnt.first else { return false }
+
+    return doMatch(prefixes: s.prefixes, with: number)
+  }
+
+  private func isGlobePrepaid(number: String) -> Bool {
+    let tnt = self.providers.filter { $0.name == "globeprepaid" }
+    var number = number
+
+    if number.hasPrefix("09") {
+      number = String(number.dropFirst())
+    }
+
+    guard let s = tnt.first else { return false }
+
+    return doMatch(prefixes: s.prefixes, with: number)
+  }
+
+  private func isGlobeTM(number: String) -> Bool {
+    let tnt = self.providers.filter { $0.name == "globetm" }
+    var number = number
+
+    if number.hasPrefix("09") {
+      number = String(number.dropFirst())
+    }
+
+    guard let s = tnt.first else { return false }
+
+    return doMatch(prefixes: s.prefixes, with: number)
+  }
+
+  private func doMatch(prefixes: [String], with number: String) -> Bool {
+    let regex = prefixes.joined(separator: "|")
+    let pattern = "(" + regex + ")"
+
+    let regexEx = try! NSRegularExpression(pattern: pattern, options: .caseInsensitive)
+    return regexEx.firstMatch(in: number, options: [], range: NSRange(location: 0, length: number.count)) != nil
   }
 }
 
 public struct VVCProvider: Decodable {
   var name: String
   var displayName: String
-  var prefixes: [Int]
+  var prefixes: [String]
 }
 
-public func loadJson(filename fileName: String) -> [VVCProvider]? {
+private func loadJson(filename fileName: String) -> [VVCProvider]? {
   let bundle = Bundle(for: VVCMobileNetwork.self)
   if let path = bundle.path(forResource: "Resources.bundle/telcos", ofType: "json") {
     do {
--- VVCPhilippinesProviderPrefixesTests/VVCPhilippinesProviderPrefixesTests.swift
+++ VVCPhilippinesProviderPrefixesTests/VVCPhilippinesProviderPrefixesTests.swift
@@ -18,9 +18,64 @@ class VVCPhilippinesProviderPrefixesTests: QuickSpec {
       it("is not Empty") {
         let a = VVCMobileNetwork()
         
-        debugPrint(a.providers)
+        debugPrint(a.providers[0].prefixes)
         expect(a.providers).notTo(beEmpty())
       }
     }
+    
+    context("Globe Network") {
+      it("is globe number") {
+        let a = VVCMobileNetwork()
+        let isGlobe = a.provider(contactNumber: "09733012090")
+
+        expect(isGlobe).to(equal(VVCMobileNetwork.NetworkProvider.globe))
+      }
+    }
+    
+    context("Sun Cell  Network") {
+      it("is Sun Cellular number") {
+        let a = VVCMobileNetwork()
+        let isGlobe = a.provider(contactNumber: "09223025090")
+        
+        expect(isGlobe).to(equal(VVCMobileNetwork.NetworkProvider.sun))
+      }
+    }
+    
+    context("Globe Prepaid") {
+      it("is Globe Prepaid number") {
+        let a = VVCMobileNetwork()
+        let isGlobe = a.provider(contactNumber: "09175012090")
+        
+        expect(isGlobe).to(equal(VVCMobileNetwork.NetworkProvider.globeprepaid))
+      }
+    }
+    
+    context("Smart Network") {
+      it("is Smart number") {
+        let a = VVCMobileNetwork()
+        let isGlobe = a.provider(contactNumber: "09283012078")
+        
+        expect(isGlobe).to(equal(VVCMobileNetwork.NetworkProvider.smart))
+      }
+    }
+    
+    context("Globe / TM") {
+      it("is GLOBE/TM number") {
+        let a = VVCMobileNetwork()
+        let isGlobe = a.provider(contactNumber: "09063012090")
+        
+        expect(isGlobe).to(equal(VVCMobileNetwork.NetworkProvider.globetm))
+      }
+    }
+    
+    context("Talk N Text Network") {
+      it("is TNT number") {
+        let a = VVCMobileNetwork()
+        let isGlobe = a.provider(contactNumber: "09483078890")
+        
+        expect(isGlobe).to(equal(VVCMobileNetwork.NetworkProvider.tnt))
+      }
+    }
   }
+
 }
